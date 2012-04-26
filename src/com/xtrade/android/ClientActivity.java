59ba16162dfaa4f80999cec9a17e01530a373eaf
Cesar Bolanos <cesar20904@gmail.com>
2012-04-26 11:33:24 +0200

    Implementing Cliente Register UI

---
+++ src/com/xtrade/android/ClientActivity.java
@@ -0,0 +1,18 @@
+package com.xtrade.android;
+
+import android.os.Bundle;
+
+/**
+ * 
+ * @author <a href="mailto:cesar20904@gmail.com">César Bolaños</a>
+ *
+ */
+public class ClientActivity extends BaseActivity {
+
+	@Override
+	public void onCreate(Bundle savedIntanceState) {
+		super.onCreate(savedIntanceState);
+		setContentView(R.layout.client);
+	}
+	
+}
--- src/com/xtrade/android/util/ActionConstant.java
+++ src/com/xtrade/android/util/ActionConstant.java
@@ -7,5 +7,6 @@ public class ActionConstant {
 	public static final String LOGIN=PREFIX+"LOGIN_ACTION";
 	public static final String CLIENT_LIST=PREFIX+"CLIENT_LIST_ACTION";
 	public static final String CLIENT_DETAIL=PREFIX+"CLIENT_DETAIL_ACTION";
+	public static final String CLIENT = PREFIX + "CLIENT_ACTION";
 	
 }
