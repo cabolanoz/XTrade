ae93b5e0a8ed198a8b311646f744f64f167c17f0
Jose Ayerdis <joseayerdis@gmail.com>
2012-08-23 23:31:48 +0200

    Added slide tab fragment functionality to contact detail.

--- src/com/xtrade/android/TraderDetailActivity.java
+++ src/com/xtrade/android/TraderDetailActivity.java
@@ -3,6 +3,8 @@ package com.xtrade.android;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.ActionBar;
@@ -11,78 +13,121 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.xtrade.android.adapter.ContactAdapter;
+import com.xtrade.android.fragment.SectionsPagerAdapter;
 import com.xtrade.android.fragment.TraderAboutFragment;
 import com.xtrade.android.fragment.TraderContactFragment;
-import com.xtrade.android.listener.TraderTabListener;
 import com.xtrade.android.provider.ContactTranslator;
 import com.xtrade.android.provider.DatabaseContract.Contact;
 import com.xtrade.android.provider.DatabaseContract.TraderColumns;
 import com.xtrade.android.util.ActionConstant;
 import com.xtrade.android.util.EventConstant;
 
-public class TraderDetailActivity extends BaseActivity implements EventConstant {
-
+public class TraderDetailActivity extends BaseActivity implements ActionBar.TabListener , EventConstant {
+	private ViewPager viewPager;
+	private SectionsPagerAdapter sectionsPagerAdapter;
 	private ActionBar actionBar;
-	
+
 	@Override
 	public void onCreate(Bundle savedIntanceState) {
-		
+
 		super.onCreate(savedIntanceState);
-		
+		setContentView(R.layout.trader);
 		// Getting the current action bar
 		actionBar = getSupportActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
-		// Trader about tab
-		Tab traderAboutTab = actionBar.newTab();
-		traderAboutTab.setTag("about");
-		traderAboutTab.setText(R.string.about);
-		traderAboutTab.setTabListener(new TraderTabListener<TraderAboutFragment>(this, "About", TraderAboutFragment.class));
-		actionBar.addTab(traderAboutTab);
-		
-		// Trader contacts tab
-		Tab traderContactTab = actionBar.newTab();
-		traderContactTab.setTag("contact");
-		traderContactTab.setText(R.string.contacts);
-		traderContactTab.setTabListener(new TraderTabListener<TraderContactFragment>(this, "Contacts", TraderContactFragment.class));
-		actionBar.addTab(traderContactTab);
+
+		sectionsPagerAdapter = new SectionsPagerAdapter(
+				getSupportFragmentManager(),
+				new Class[] { TraderAboutFragment.class,
+						TraderContactFragment.class },
+				new String[] { getString(R.string.about),
+						getString(R.string.contacts) }, this);
+
+		// Set up the ViewPager with the sections adapter.
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		viewPager.setAdapter(sectionsPagerAdapter);
+
+		viewPager
+				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+					@Override
+					public void onPageSelected(int position) {
+						actionBar.setSelectedNavigationItem(position);
+					}
+				});
+
+		for (int i = 0; i < sectionsPagerAdapter.getCount(); i++) {
+			// Create a tab with text corresponding to the page title defined by
+			// the adapter.
+			// Also specify this Activity object, which implements the
+			// TabListener interface, as the
+			// listener for when this tab is selected.
+			actionBar.addTab(actionBar.newTab()
+					.setText(sectionsPagerAdapter.getPageTitle(i))
+					.setTabListener(this));
+		}
+
 	}
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		
+
 		if (resultCode == RESULT_CANCELED)
 			return;
-		
-		if (resultCode == RESULT_OK && requestCode == CONTACT_CREATE_REQUEST_CODE) {
+
+		if (resultCode == RESULT_OK
+				&& requestCode == CONTACT_CREATE_REQUEST_CODE) {
 			ListView lvwContact = (ListView) findViewById(R.id.lvwContact);
 			if (lvwContact != null) {
-				Cursor cursor = getContentResolver().query(Contact.CONTENT_URI, null, null, null, null);
-				((ContactAdapter) lvwContact.getAdapter()).setContactList(new ContactTranslator().translate(cursor));
+				Cursor cursor = getContentResolver().query(Contact.CONTENT_URI,
+						null, null, null, null);
+				((ContactAdapter) lvwContact.getAdapter())
+						.setContactList(new ContactTranslator()
+								.translate(cursor));
 			}
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu _menu) {
 		MenuInflater menuInflater = getSupportMenuInflater();
 		menuInflater.inflate(R.menu.trader_tab_contact_menu, _menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem _menuItem) {
 		switch (_menuItem.getItemId()) {
 		case R.id.mniNewContact:
 			Intent intent = new Intent(ActionConstant.CONTACT_CREATE_UPDATE);
 			intent.putExtra("ACTION_TYPE", CONTACT_CREATE_REQUEST_CODE);
-			intent.putExtra(TraderColumns.TRADER_ID, getIntent().getStringExtra(TraderColumns.TRADER_ID));
+			intent.putExtra(TraderColumns.TRADER_ID, getIntent()
+					.getStringExtra(TraderColumns.TRADER_ID));
 			startActivityForResult(intent, CONTACT_CREATE_REQUEST_CODE);
 			return true;
 		default:
-			return super.onOptionsItemSelected(_menuItem);		
+			return super.onOptionsItemSelected(_menuItem);
 		}
 	}
 	
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		viewPager.setCurrentItem(tab.getPosition());
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
