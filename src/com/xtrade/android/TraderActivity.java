b9c429e49dd25ce324758fddb9d775d5973cd40a
Jose Ayerdis <joseayerdis@gmail.com>
2012-08-23 03:25:16 +0200

    Viewpager behaviour for Trader tabs

--- src/com/xtrade/android/TraderActivity.java
+++ src/com/xtrade/android/TraderActivity.java
@@ -3,6 +3,11 @@ package com.xtrade.android;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.ActionBar;
@@ -18,28 +23,41 @@ import com.xtrade.android.provider.TraderTranslator;
 import com.xtrade.android.util.ActionConstant;
 import com.xtrade.android.util.EventConstant;
 
-public class TraderActivity extends BaseActivity implements EventConstant {
+public class TraderActivity extends BaseActivity implements ActionBar.TabListener, EventConstant {
 
+	
+	private ViewPager viewPager;
+	private SectionsPagerAdapter sectionsPagerAdapter;
+	
 	public void onCreate(Bundle savedIntanceState) {
 		super.onCreate(savedIntanceState);
-		
+		setContentView(R.layout.trader);
 		// Getting the current action bar
-		ActionBar actionBar = getSupportActionBar();
+		final ActionBar actionBar = getSupportActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		
-		// Trader list tab
-		Tab traderListTab = actionBar.newTab();
-		traderListTab.setTag("list");
-		traderListTab.setText(R.string.traders);
-		traderListTab.setTabListener(new TraderTabListener<TraderListFragment>(this, "Trader", TraderListFragment.class));
-		actionBar.addTab(traderListTab);
+		 sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());	       
+
+	    // Set up the ViewPager with the sections adapter.
+	    viewPager = (ViewPager) findViewById(R.id.pager);
+	    viewPager.setAdapter(sectionsPagerAdapter);
+	    
+	    viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
 		
-		// Trader today tab
-		Tab traderTodayTab = actionBar.newTab();
-		traderTodayTab.setTag("today");
-		traderTodayTab.setText(R.string.today);
-		traderTodayTab.setTabListener(new TraderTabListener<TraderTodayFragment>(this, "Today", TraderTodayFragment.class));
-		actionBar.addTab(traderTodayTab);
+	    for (int i = 0; i < sectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by the adapter.
+            // Also specify this Activity object, which implements the TabListener interface, as the
+            // listener for when this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(sectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
 	}
 
 	@Override
@@ -57,6 +75,45 @@ public class TraderActivity extends BaseActivity implements EventConstant {
 			}
 		}
 	}
+	
+	/**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
+     * sections of the app.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch(i){
+            case 0:
+            	return Fragment.instantiate(TraderActivity.this, TraderListFragment.class.getName());
+            case 1:
+            	return Fragment.instantiate(TraderActivity.this, TraderTodayFragment.class.getName());
+            
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+            //TODO: this should go to string.xml
+                case 0: return "Traders";
+                case 1: return "Events";
+            }
+            return null;
+        }
+    }
+
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,6 +139,24 @@ public class TraderActivity extends BaseActivity implements EventConstant {
 		}
 		
 		return super.onOptionsItemSelected(menuItem);
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
