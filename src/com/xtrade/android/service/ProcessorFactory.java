1ab6a954de798a6453467f5a355e5085a14a4535
Jose Ayerdis <joseayerdis@gmail.com>
2012-09-02 04:16:48 +0200

    Create processor class for interpretate JSON data.

---
+++ src/com/xtrade/android/service/ProcessorFactory.java
@@ -0,0 +1,17 @@
+package com.xtrade.android.service;
+
+import android.content.Context;
+
+import com.xtrade.android.util.ActionConstant;
+
+public class ProcessorFactory {
+
+	public static ProcessorBase getProcessor(String action,Context context) {
+
+		if(action.equals(ActionConstant.REQUEST_DATA))
+			return new TraderProcessor(context);
+		
+		return null;
+	}
+
+}
