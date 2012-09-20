3cbee8fe9654b8dd3c5341d4737a6cc3d1c3e208
Jose Ayerdis <joseayerdis@gmail.com>
2012-09-20 02:43:59 +0200

    Refactor REST architecture and calls

--- src/com/xtrade/android/http/RestMethod.java
+++
@@ -1,5 +0,0 @@
-package com.xtrade.android.http;
-
-public enum RestMethod {
-	GET,PUT,POST,DELETE;	
-}
---
+++ src/com/xtrade/android/http/RestOption.java
@@ -0,0 +1,7 @@
+package com.xtrade.android.http;
+
+public class RestOption{
+	
+	public enum Method{GET,PUT,POST,DELETE};
+	public enum Parameter{BODY};
+}
