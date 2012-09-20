2a2bd4bd07445e26e5fcb5ec132289c3c2590788
Jose Ayerdis <joseayerdis@gmail.com>
2012-09-20 02:43:30 +0200

    Add new object for Authentication

---
+++ src/com/xtrade/android/object/User.java
@@ -0,0 +1,14 @@
+package com.xtrade.android.object;
+
+public class User {
+
+	public User(String username, String password){
+		this.username=username;
+		this.password=password;
+	}
+	
+	public String username;
+	public String password;
+	public String cookie;
+	
+}
